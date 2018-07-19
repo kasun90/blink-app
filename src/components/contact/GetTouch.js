03601efccdeeaf6a6c2c9838aa5e79ad705c0955
Kasun Piyumal <p.kasun@directfn.com>
2018-07-19 18:14:00 +0530

    modal and button changes

--- src/components/contact/GetTouch.js
+++ src/components/contact/GetTouch.js
@@ -11,35 +11,82 @@ class GetTouch extends WithNetwork {
 
     constructor(props) {
         super(props);
-        this.name = "";
-        this.phoneNumber = "";
-        this.message = "";
+        this.state = {
+            name: "",
+            phoneNumber: "",
+            message: "",
+            email: "",
+            showModal: false,
+            modalMessage: "",
+            isSending: false
+        }
         this.onSend = this.onSend.bind(this);
         this.onNameChange = this.onNameChange.bind(this);
         this.onPhoneNumberChange = this.onPhoneNumberChange.bind(this);
         this.onMessageChange = this.onMessageChange.bind(this);
+        this.onEmailChange = this.onEmailChange.bind(this);
+        this.onModalClose = this.onModalClose.bind(this);
     }
 
     onSend() {
         var msg = WithNetwork.buildMessage('com.blink.shared.client.messaging.UserMessage');
-        msg.message = this.message;
+        msg.message = this.state.message;
+        msg.name = this.state.name;
+        msg.email = this.state.email;
+        msg.phone = this.state.phoneNumber;
+        this.setState({
+            isSending: true
+        });
         this.send(msg, (response, error) => {
             if (error !== undefined) {
+                this.setState({
+                    modalMessage: "Network Error",
+                    showModal: true,
+                    isSending: false
+                });
+            } else {
+                this.setState({
+                    name: "",
+                    phoneNumber: "",
+                    message: "",
+                    email: "",
+                    modalMessage: "Your message has been sent",
+                    showModal: true,
+                    isSending: false
+                })
                 
             }
         });
     }
 
     onNameChange(event) {
-        this.name = event.target.value;
+        this.setState({
+            name: event.target.value
+        });
     }
 
     onPhoneNumberChange(event) {
-        this.phoneNumber = event.target.value;
+        this.setState({
+            phoneNumber: event.target.value
+        });
+    }
+
+    onEmailChange(event) {
+        this.setState({
+            email: event.target.value
+        });
     }
 
     onMessageChange(event) {
-        this.message = event.target.value;
+        this.setState({
+            message: event.target.value
+        });
+    }
+
+    onModalClose() {
+        this.setState({
+            showModal: false
+        });
     }
 
 
@@ -54,13 +101,14 @@ class GetTouch extends WithNetwork {
                 <div className="GetTouch-sub-header">Email me<br/>kpiyumal90@gmail.com<br/><br/>Call me or Text me<br/>+94 71 331 7596
                 <br/><br/>or<br/>Send me a message<br/></div>
                 <div className="GetTouch-form-submit-container">
-                    <BlinkTextField className="GetTouch-sub-header" type="text" placeholder="Name" onChange={this.onNameChange}/>
-                    <BlinkTextField className="GetTouch-sub-header" type="text" placeholder="Phone Number" onChange={this.onPhoneNumberChange}/>
-                    <textarea className={`GetTouch-form-text-area Blink-outline`} placeholder="Type Your Message Here" onChange={this.onMessageChange}></textarea>
-                    <BlinkButton className="GetTouch-sub-header" onClick={this.onSend}>Send</BlinkButton>
+                    <BlinkTextField className="GetTouch-sub-header" type="text" placeholder="Name" value={this.state.name} onChange={this.onNameChange}/>
+                    <BlinkTextField className="GetTouch-sub-header" type="text" placeholder="Phone Number" value={this.state.phoneNumber} onChange={this.onPhoneNumberChange}/>
+                    <BlinkTextField className="GetTouch-sub-header" type="text" placeholder="Email" value={this.state.email} onChange={this.onEmailChange}/>
+                    <textarea className={`GetTouch-form-text-area Blink-outline`} placeholder="Type Your Message Here" value={this.state.message} onChange={this.onMessageChange}></textarea>
+                    <BlinkButton className="GetTouch-sub-header" onClick={this.onSend} disabled={this.state.isSending}>Send</BlinkButton>
                 </div>
             </div>
-            <BlinkModal message="Message recorded and fine" isOpen={true}/>
+            <BlinkModal message={this.state.modalMessage} isOpen={this.state.showModal} onClose={this.onModalClose}/>
         </div>);
     }
 }
