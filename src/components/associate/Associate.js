b1306ff62f1ea50e87e67b711854fed99d7f7501
Kasun Piyumal <p.kasun@directfn.com>
2018-07-31 18:34:44 +0530

    experimenting associate

--- src/components/associate/Associate.js
+++ src/components/associate/Associate.js
@@ -19,11 +19,11 @@ class Associate extends Component {
                 <iframe frameBorder="0" title="sdf" className="Associate-video"
                 src="https://www.youtube.com/embed/rxTgDK-disY?rel=0&amp;autoplay=1&loop=1&playlist=rxTgDK-disY&controls=0&showinfo=0&autohide=1&mute=1" allowFullScreen></iframe>
             </div>
-            <div className="Associate-empty"/>
             <div className="Associate-paragraph">
                 Nubamin is the associate of Blink. And here is the showcase of him
             </div>
             <div className="Associate-parallax" style={{backgroundImage: `url(${aero})`}}/>
+            <div className="Associate-empty"/>
             <div className="Associate-paragraph">
                 That aero is wow right? Like to contact him?
             </div>
