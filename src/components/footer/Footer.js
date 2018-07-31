47ddd767e7c0d17b8cbb2f734b29fc2a7aadc2bd
Kasun Piyumal <p.kasun@directfn.com>
2018-07-31 11:45:53 +0530

    footer link with href

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -7,32 +7,19 @@ import Ionicon from 'react-ionicons';
 
 class Footer extends Component {
 
-    constructor(props) {
-        super(props);
-        this.onFacebookClick = this.onFacebookClick.bind(this);
-        this.onInstagramClick = this.onInstagramClick.bind(this);
-        this.onYoutubeClick = this.onYoutubeClick.bind(this);
-    }
-
-    onFacebookClick() {
-        window.open("https://www.facebook.com/kpiyumal", "_blank");
-    }
-
-    onInstagramClick() {
-        window.open("https://www.instagram.com/kazun90/", "_blank");
-    }
-
-    onYoutubeClick() {
-        window.open("https://www.youtube.com/channel/UCCEyBUbNpaHQxgcxX1dNkTQ", "_blank");
-    }
-
     render() {
         return (
         <div className={`Footer Background`}>
             <div className="Icon-container">
-                <Ionicon icon="logo-facebook" className="Icon" fontSize="35px" color={Colors.Blink} onClick={this.onFacebookClick}/>
-                <Ionicon icon="logo-instagram" className="Icon" fontSize="35px" color={Colors.Blink} onClick={this.onInstagramClick}/>
-                <Ionicon icon="logo-youtube" className="Icon" fontSize="35px" color={Colors.Blink} onClick={this.onYoutubeClick}/>
+                <a href="https://www.facebook.com/kpiyumal" rel="noopener noreferrer" target="_blank">
+                    <Ionicon icon="logo-facebook" className="Icon" fontSize="35px" color={Colors.Blink}/>
+                </a>
+                <a href="https://www.instagram.com/kazun90/" rel="noopener noreferrer" target="_blank">
+                    <Ionicon icon="logo-instagram" className="Icon" fontSize="35px" color={Colors.Blink}/>
+                </a>
+                <a href="https://www.youtube.com/channel/UCCEyBUbNpaHQxgcxX1dNkTQ" rel="noopener noreferrer" target="_blank">
+                    <Ionicon icon="logo-youtube" className="Icon" fontSize="35px" color={Colors.Blink}/>
+                </a>
             </div>
             <div className={`Copyright Blink`}>
                 © 2018 Blink
