9fa5ab5de427cbdc307f30c7a61e7007f1573eef
Kasun Piyumal <kpiyumal90@gmail.com>
2018-08-02 00:27:27 +0530

    making associate responsive

--- src/components/associate/Associate.js
+++ src/components/associate/Associate.js
@@ -5,28 +5,37 @@ import aero from './img/aero.jpg';
 import paddy from './img/paddy.jpg';
 import dondra from './img/dondra.jpg';
 import beach from './img/beach.jpg';
+import profile from './img/profile.jpg';
 
 class Associate extends Component {
 
     componentDidMount() {
         window.scroll(0,0);
-        // rxTgDK-disY
     }
 
     render() {
         return(
         <div className={`Associate-main-container Blink`}>
-            <div className="Associate-video-container">
-                <iframe frameBorder="0" title="sdf" className="Associate-video"
-                src="https://www.youtube.com/embed/DvwnuFhvlYA?rel=0&amp;autoplay=1&loop=1&playlist=DvwnuFhvlYA&controls=0&showinfo=0&autohide=1&mute=1" allowFullScreen></iframe>
-            </div>
-            <div className="Associate-paragraph">
-                Nubamin is the associate of Blink. And here is the showcase of him
+            <div className="Associate-profile-container">
+                <div className="Associate-profile-image-container">
+                    <img src={profile} className="Associate-profile-image" alt="profile"/>
+                </div>
+                <div className="Associate-profile-text"><span>Nubamin</span>
+                <br/>Pradeep Liyanage
+                <br/>A videographer, <i>a dreamer</i>
+                <br/><br/>+94 77 732 4954
+                <br/>nubamin@gmail.com
+                </div>
             </div>
             <div className="Associate-parallax" style={{backgroundImage: `url(${aero})`}}/>
-            <div className="Associate-empty"/>
-            <div className="Associate-paragraph">
-                That aero is wow right? Like to contact him?
+            <div className="Associate-main-video">
+                <div className="Associate-main-video-container">
+                    <div style={{textAlign: 'center', fontSize: '2em', marginBottom: '1em'}}>His journey to up country</div>
+                    <div className="Associate-video-container">
+                        <iframe frameBorder="0" title="sdf" className="Associate-video"
+                        src="https://www.youtube.com/embed/rxTgDK-disY?rel=0&amp;autoplay=0&loop=1&playlist=rxTgDK-disY&controls=1&showinfo=0&autohide=1&mute=1"></iframe>
+                    </div>
+                </div>
             </div>
             <div className="Associate-parallax" style={{backgroundImage: `url(${paddy})`}}/>
             <div className="Associate-paragraph">
