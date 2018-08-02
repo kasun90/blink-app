f4cde1106c9b332d8aad5bd3ddeb3240e99fd06e
Kasun Piyumal <p.kasun@directfn.com>
2018-08-02 16:48:13 +0530

    description updated

--- src/components/associate/Associate.js
+++ src/components/associate/Associate.js
@@ -22,7 +22,7 @@ class Associate extends Component {
                 </div>
                 <div className="Associate-profile-text"><span>Nubamin</span>
                 <br/>Pradeep Liyanage
-                <br/>A videographer, <i>a dreamer</i>
+                <br/>A content creator, <i>a dreamer</i>
                 <br/><br/>+94 77 732 4954
                 <br/>nubamin@gmail.com
                 </div>
@@ -30,20 +30,32 @@ class Associate extends Component {
             <div className="Associate-parallax" style={{backgroundImage: `url(${aero})`}}/>
             <div className="Associate-main-video">
                 <div className="Associate-main-video-container">
-                    <div style={{textAlign: 'center', fontSize: '2em', marginBottom: '1em'}}>His journey to up country</div>
+                    <div className="Associate-video-title">Exploring hill country</div>
                     <div className="Associate-video-container">
                         <iframe frameBorder="0" title="sdf" className="Associate-video"
-                        src="https://www.youtube.com/embed/rxTgDK-disY?rel=0&amp;autoplay=0&loop=1&playlist=rxTgDK-disY&controls=1&showinfo=0&autohide=1&mute=1"></iframe>
+                        src="https://www.youtube.com/embed/rxTgDK-disY?rel=0&amp;autoplay=0&loop=1&playlist=rxTgDK-disY&controls=1&showinfo=0&autohide=1&mute=1" allowFullScreen></iframe>
                     </div>
                 </div>
             </div>
             <div className="Associate-parallax" style={{backgroundImage: `url(${paddy})`}}/>
             <div className="Associate-paragraph">
-                That aero is wow right? Like to contact him?
+                He is from my area too, Walasmulla. Yes, He has a drone. And he is good at creating
+                content with it. He undertake events and projects. If you think your project can 
+                benefit from drone shots and stills from a handheld at the same time, then you might
+                as well reach out to him.
+                <br/><br/>
+                He is fun to work with. He has tons of creative ideas. Check out his facebook page,
+                <a href="https://www.facebook.com/nubamin" style={{color: 'inherit'}}>Nubamin</a> for
+                 more awesome stuff and to say updated.
+                
             </div>
             <div className="Associate-parallax" style={{backgroundImage: `url(${dondra})`}}/>
             <div className="Associate-paragraph">
-                Wanna see more?
+            He is working for content creating companies and freelancing most of the time. So he has 
+            the experience.
+            <br/>
+                You want both of us to be in the same project? That can be arranged too. Also we would love 
+                to hear your ideas and build a project upon them.
             </div>
             <div className="Associate-parallax" style={{backgroundImage: `url(${beach})`}}/>
         </div>
