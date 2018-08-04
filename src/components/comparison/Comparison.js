b8b4947fee7974c809dc99fd1902ba9c9c89bbf3
Kasun Piyumal <kpiyumal90@gmail.com>
2018-08-05 02:39:12 +0530

    comparison stage 2

--- src/components/comparison/Comparison.js
+++ src/components/comparison/Comparison.js
@@ -3,12 +3,19 @@ import './Comparison.css';
 
 class Comparison extends Component {
 
+    x = 0;
+    width = 0;
+    afterImage = undefined;
+
     constructor(props) {
         super(props);
         this.onResize = this.onResize.bind(this);
+        this.onMouseEnter = this.onMouseEnter.bind(this);
     }
 
     componentDidMount() {
+        this.setBackgroundImage();
+        this.setBackgroundSize();
         window.addEventListener('resize', this.onResize);
     }
 
@@ -16,14 +23,37 @@ class Comparison extends Component {
         window.removeEventListener('resize', this.onResize);
     }
 
+    setBackgroundImage() {
+        let afterImage = document.getElementById("comparison-after");
+        afterImage.style.background = `url(${this.props.after})`;
+        afterImage.style.backgroundPosition = "center right";
+    }
+
+    setBackgroundSize() {
+        let container = document.getElementById("comparison-container");
+        let afterImage = document.getElementById("comparison-after");
+
+        afterImage.style.backgroundSize = `${container.clientWidth}px ${container.clientHeight}px`;
+    }
+
+    onMouseEnter() {
+        let containerRect = document.getElementById("comparison-container").getBoundingClientRect();
+        this.x = containerRect.x;
+        this.width = containerRect.width;
+        this.afterImage = document.getElementById("comparison-after");
+    }
+
     onResize() {
-        console.log("on resize");
+        this.setBackgroundSize();
     }
 
     render() {
-        return(<div className="Comparison-container">
-            <div className="Comparison-image"/>
-            <div className="Comparison-image"/>
+        return(<div onMouseEnter={this.onMouseEnter} onMouseMove={event => {
+            const horizontal = ((event.clientX - this.x) / this.width) * 100
+		    this.afterImage.style.setProperty('--x', horizontal + '%')
+        }} id="comparison-container" className="Comparison-container">
+            <div id="comparison-after" className="Comparison-image-after"/>
+            <img className="Comparison-image-before" src={this.props.before} alt="before"/>
         </div>);
     }
 }
