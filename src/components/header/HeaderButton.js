87ffe45c1b35d231b4f61c972661cb22d4a9003a
Kasun Piyumal <kpiyumal90@gmail.com>
2018-08-07 00:44:01 +0530

    header added

--- src/components/header/HeaderButton.js
+++ src/components/header/HeaderButton.js
@@ -5,13 +5,39 @@ import {NavLink} from 'react-router-dom';
 
 class HeaderButton extends Component {
     render() {
-        return (
-            <NavLink style={{ textDecoration: 'none', color: 'inherit'}} to={this.props.path}> 
-            <div className="Header-button">
-                {this.props.name}
-            </div>
-            </NavLink>
-        );
+
+        if (this.props.data.dropDown) {
+
+            const dropButtons = [];
+
+            for(var i = 0; i < this.props.data.children.length; i++) {
+                const childData = this.props.data.children[i];
+                dropButtons.push(
+                    <NavLink style={{ textDecoration: 'none', color: 'inherit'}} to={childData.path}  key={i}> 
+                        <div>{childData.name}</div>
+                    </NavLink>
+                );
+            }
+            
+            return(
+                <div className="Header-dropdown">
+                    <div className="Header-button">
+                        {this.props.data.name}
+                    </div>
+                    <div className={`Header-dropdown-content Blink-background`}>
+                        {dropButtons}
+                    </div>
+                </div>
+            );
+        } else {
+            return (
+                <NavLink style={{ textDecoration: 'none', color: 'inherit'}} to={this.props.data.path}> 
+                <div className="Header-button">
+                    {this.props.data.name}
+                </div>
+                </NavLink>
+            );
+        }
     }
 }
 
