6adbda9ca77e44dc06c23b72264f959c801e6ba8
Kasun Piyumal <p.kasun@directfn.com>
2018-08-24 14:43:23 +0530

    presets back end support

--- src/components/presets/PresetElement.js
+++ src/components/presets/PresetElement.js
@@ -6,9 +6,9 @@ import BlinkButton from './../blinkbutton/BlinkButton';
 class PresetElement extends Component {
     render() {
         return(<div className="PresetEle-container">
-            <div className="PresetEle-name">{this.props.data.name}</div>
+            <div className="PresetEle-name">{this.props.data.title}</div>
             <div style={{marginTop: '1vh', marginBottom: '1vh'}}>
-                <Comparison before={this.props.data.before} after={this.props.data.after}/>
+                <Comparison before={this.props.data.beforeImage.url} after={this.props.data.afterImage.url}/>
             </div>
             <BlinkButton>Download</BlinkButton>
         </div>);
