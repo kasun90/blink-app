c49262f91e534d141ce655bd26827b5ac3e1585a
Kasun Piyumal <kpiyumal90@gmail.com>
2018-08-31 17:00:52 +0530

    description update

--- src/components/contact/AboutMe.js
+++ src/components/contact/AboutMe.js
@@ -10,7 +10,7 @@ class AboutMe extends Component {
             <div className={`AboutMe-text-container Blink`}>
                 <div className="AboutMe-header">Hi, I'm Kasun</div>
                 <div className="AboutMe-paragraph">
-                    Photography is my passionate hobby. I develop software for a living.<br/><br/>
+                    Photography is my passionate hobby. I develop softwares for a living.<br/><br/>
                     I love portraits. I enjoy outdoors and natural light too.
                     For me, a photograph is about a fraction of time in our lives. It is a memory. 
                     My ambition is to create a memory that people can cherish for a life time.<br/><br/>
