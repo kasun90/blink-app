edac1d1dc45752a370d8ecfd6b7c814f2303cc77
Kasun Piyumal <kpiyumal90@gmail.com>
2018-12-08 21:10:01 +0530

    closes #16

--- src/components/blinkImage/BlinkImage.js
+++ src/components/blinkImage/BlinkImage.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import './BlinkImage.css';
+import './../../common/Colors.css';
 
 class BlinkImage extends Component {
 
     isImageMounted = true;
+    placeholderEnabled= false;
 
     constructor(props) {
         super(props);
+        this.placeholderEnabled = this.props.placeholderEnabled !== undefined && this.props.placeholderEnabled;
         this.state = {
-            imageLoad : ''
+            imageLoad : '',
+            placeHolderClass: (this.placeholderEnabled) ? 'Background-x Blink-image-placeholder' : ''
         }
         this.onImageLoad = this.onImageLoad.bind(this);
     }
@@ -16,7 +20,8 @@ class BlinkImage extends Component {
     onImageLoad() {
         if (this.isImageMounted) {
             this.setState({
-                imageLoad: 'Blink-image-load'
+                imageLoad: this.placeholderEnabled ? 'Blink-image-with-placeholder-load' : 'Blink-image-without-placeholder-load',
+                placeHolderClass: ''
             });
         }
     }
@@ -30,8 +35,9 @@ class BlinkImage extends Component {
     }
 
     render() {
-        return(<div className={`${this.props.className}`}>
-            <img className={`Blink-image-image ${this.state.imageLoad}`} src={this.props.src} alt="blink" onLoad={this.onImageLoad}/>
+        const imageClass = this.placeholderEnabled ? 'Blink-image-with-placeholder' : 'Blink-image-without-placeholder';
+        return(<div className={`${this.props.className} ${this.state.placeHolderClass}`}>
+            <img className={`Blink-image-image ${imageClass} ${this.state.imageLoad}`} src={this.props.src} alt="blink" onLoad={this.onImageLoad}/>
         </div>);
     }
 }
