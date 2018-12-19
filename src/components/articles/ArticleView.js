58a85e1945b8309e92f6ec1e65b9c493510c95de
Kasun Piyumal <kpiyumal90@gmail.com>
2018-12-19 18:09:15 +0530

    share buttong added for articles and albums, closes #21

--- src/components/articles/ArticleView.js
+++ src/components/articles/ArticleView.js
@@ -87,6 +87,7 @@ class ArticleView extends WithNetwork {
             <div className="ArticleView-container Blink-paragraph">
                 {atags}
             </div>
+            <div class="fb-share-button" data-href={window.location.href} data-layout="button_count" data-size="small" data-mobile-iframe="true"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Share</a></div>
             <div className="fb-comments" data-href={window.location.href} data-numposts="5" data-width="100%"></div>
         </div>);
     }
