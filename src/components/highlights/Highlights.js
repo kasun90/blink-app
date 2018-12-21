4d863896783a7921f96a8ba9b5f2e244a9584a58
Kasun Piyumal <kpiyumal90@gmail.com>
2018-12-21 17:18:13 +0530

    highlight changes

--- src/components/highlights/Highlights.js
+++ src/components/highlights/Highlights.js
@@ -13,36 +13,29 @@ class Highlights extends Component {
         return (
             
         <div className="Highlight-container">
-            <div className={`Explore Blink`}>
-                Let's explore! :)
-            </div>
             <div className="Story-container">
-                <div className="Image-container" id="image">
-                    <BlinkImage className="Image" src={nature}/>
-                </div>
-                <div className="Arrow-container" id="arrow">
-                    <Ionicon icon="md-arrow-dropdown-circle" fontSize="2em" color={Colors.Blink}/>
-                </div>
-                <div className={`Text-container Blink`} id="text">
-                    <div className={`Right-text Title`}>
-                        Fields of Ampara
-                    </div>
+                <div className="Content-container" style={{backgroundImage: `url(${nature})`}}>
+                    <div className={`Center-text Title`}>Fields of Ampara</div>
                     <div className="Seperator"></div>
-                    <div className={`Right-text Description`}>Found this on the way to my trip to Amapara with friends. 
-                    You can see this place while you travel via the scenic Lahugala road.</div>
+                    <div className={`Center-text Description`}>Found this on the way to my trip to Amapara with friends. 
+                        You can see this place while you travel via the scenic Lahugala road.</div>
                 </div>
-                
             </div>
-            <div className="Story-container">
-                <div className={`Text-container Blink`} id="text">
+        </div>
+        );
+    }
+}
+
+export default Highlights;
+
+
+/* <div className="Story-container">
+                <div className={`Text-container`} style={{color: "white"}} id="text">
                     <div className={`Left-text Title`}>
                         I'm in to portraits!
                     </div>
                     <div className="Seperator"></div>
                     <div className={`Left-text Description`}>Get your portraits done. Choose the style you want. Procedure has never been more flexible. Book your session now</div>
-                </div>
-                <div className="Arrow-container" id="arrow">
-                    <Ionicon icon="md-arrow-dropdown-circle" fontSize="2em" color={Colors.Blink}/>
                 </div>
                 <div className="Image-container" id="image">
                     <BlinkImage className="Image" src={fashion}/>
@@ -52,9 +45,6 @@ class Highlights extends Component {
                 <div className="Image-container" id="image">
                     <BlinkImage className="Image" src={nature2}/>
                 </div>
-                <div className="Arrow-container" id="arrow">
-                    <Ionicon icon="md-arrow-dropdown-circle" fontSize="2em" color={Colors.Blink}/>
-                </div>
                 <div className={`Text-container Blink`} id="text">
                     <div className={`Right-text Title`}>
                         I love nature!
@@ -63,10 +53,4 @@ class Highlights extends Component {
                     <div className={`Right-text Description`}>Cold weather and hiking. That is one perfect combination. Captured this at Badulla</div>
                 </div>
                 
-            </div>
-        </div>
-        );
-    }
-}
-
-export default Highlights;
+            </div> */
