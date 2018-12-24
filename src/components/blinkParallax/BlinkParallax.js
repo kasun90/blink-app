e5052a348aefcd0b082272c762f94a4691f110ef
Kasun Piyumal <kpiyumal90@gmail.com>
2018-12-24 17:19:40 +0530

    parallax testing

--- src/components/blinkParallax/BlinkParallax.js
+++ src/components/blinkParallax/BlinkParallax.js
@@ -18,13 +18,28 @@ class BlinkParallax extends Component {
 
     onPageScroll() {
         const element  = document.getElementById('bp-main');
-        console.log('main: ' +  document.documentElement.scrollTop + ' element: ' + element.offsetTop + ' windowHeight: ' +  window.innerHeight)
+        const contentElemet = document.getElementById('bp-content');
+        const totalScrollAmount = window.innerHeight + element.clientHeight;
+        var scrollAmount = 0;
+
+        if (document.body.scrollTop !== 0) {
+            scrollAmount = document.body.scrollTop;
+        } else {
+            scrollAmount = document.documentElement.scrollTop;
+        }
+
+        const currentScrollOfDiv = scrollAmount + window.innerHeight - element.offsetTop;
+        if (currentScrollOfDiv >= 0) {
+            var scrollPercentage = (currentScrollOfDiv * 100 / totalScrollAmount).toFixed(2);
+            contentElemet.style.backgroundPositionY = (100 - scrollPercentage) + '%';
+            console.log('Scrolled percentage: ' + scrollPercentage);
+        }
     }
 
     render() {
         return (
         <div id="bp-main" className="bp-main-container">
-            <div className="bp-content-container" style={{backgroundImage: `url(${this.props.background})`}}>
+            <div id="bp-content" className="bp-content-container" style={{backgroundImage: `url(${this.props.background})`}}>
                 <div className="bp-center-text bp-title">{this.props.data.title}</div>
                 <div className="seperator"></div>
                 <div className="bp-center-text bp-description">{this.props.data.description}</div>
